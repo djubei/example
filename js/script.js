d89c1a607baaff5536d3c86e49d6744cbd79197b
Max <djubaiy@gmail.com>
2022-01-29 22:28:03 +0300

    ноnew rep

---
+++ js/script.js
@@ -0,0 +1,69 @@
+/*console.log(`1`)
+
+ let result = confirm('enter smg') // подтверждение на сайте
+ console.log(result)*/
+
+//==================================================================================
+
+/*let result = prompt('enter smg', '11')
+// команда для запроса получения какой-либо инфы от пользователя на сатйе возвращет
+// тип данных строка
+console.log(result)
+console.log(typeof (result))*/
+
+//==================================================================================
+
+/*let resultNumber = +prompt('enter smg', '11')
+// возвращает резултат с типом данных намбр если поставить + перед prompt*/
+
+//==================================================================================
+
+/*
+let answers = []
+
+const getAnswers = (question) => {
+    let answer = prompt(question, '')
+    answers.push(answer)
+    console.log(answers)
+}
+getAnswers('как дела')
+getAnswers('как тебя зовут')
+getAnswers('сколько времени?')
+
+document.write(answers)// выводит на странице значения
+console.log(typeof (undefined))*/
+
+//==================================================================================
+
+/*
+//унарный плюс преобразует все в числа тоже самое что и Number()
+console.log(+'a')
+console.log(Number(true))
+console.log(Number(`2`))//строку нельзя преобразовать в число , если это не строка с числом "1"
+console.log(Number(`2`))
+let a = (1 + 2, 3 + 4)
+console.log(a)
+console.log("" + 1 + 0)
+console.log(" \t \n" - 2)
+let b = 1, c = 4, e = 10, m = 8
+console.log(b++)
+console.log(c--)
+console.log(++e)
+console.log(--m)
+*/
+
+//==================================================================================
+//git init -инициализация репозитория
+//git config --global user.name "Max" имя
+//git config --global user.email "djubaiy@gmail.com" имя
+//git status посмотреть статус файлов проекта
+//git add -A добавить файлы в index
+//git add *.js добавить файлы c окончанием .js в index
+//git commit -a -m""   добавить файлы в репозиторий -а - значит все -m значит комментарий к коммиту
+
+
+
+
+
+
+
